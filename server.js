d158135d038a7f9b6b8d00a5f88fd27e3ea32152
s22235581 <s222355812@deakin.edu.au>
2022-08-01 13:19:26 +1000

    commit

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 var express= require("express")
 var app=express()
-app.use(express.static(__dirname+'/public'))
+
+app.use(express.static(__dirname+'/public'));
 app.use(express.json());
 app.use(express.urlencoded({extends:false}));
 
-//add number
-const addNumbers = (num1, num2) => {​
-
-    var number1 = parseInt(num1)
-    var number2 = parseInt(num2)
-    var result = number1 + number2;​
-    return result; ​
-    
+const addTwoNums = (num1, num2) => {
+   var number1=parseInt(num1);
+   var number2=parseInt(num2);
+   var result= number1+number2;
+   return result;
     }
-
-    app.get ("/addTwoNumbers", (req,res) => {​
+    app.get("/addTwoNumbers",(req,res) =>{​
 
         var num1 = req.query.num1;​
         var num2 = req.query.num2; ​
-        var result = addNumbers(num1, num2);​
+        var res = addNumbers(num1, num2);​
         res.json({statusCode: 200, data: result, message:"Success"})​
         
     })
 
 var port=process.env.port||3000;
+
 app.listen(port,()=>{
-console.log("listening to"+port)
+    console.log("listening to"+port)
 })
