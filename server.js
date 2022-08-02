9b4294daba5bc5af6990df7c5fa4788664b91e37
s222355812 <109494663+s222355812@users.noreply.github.com>
2022-08-02 22:00:29 +1000

    Update server.js
    put added

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ app.use(express.static(__dirname+'/public'));
 app.use(express.json());
 app.use(express.urlencoded({extends:false}));
 
+// method for 2 numbers
 const addTwoNums = (num1, num2) => {
    var number1=parseInt(num1);
    var number2=parseInt(num2);
    var result= number1+number2;
    return result;
     }
+
     //REST - GET (using query param)
     app.get("/addTwoNumbers",(req,res)=>{
         var num1=req.query.num1;
@@ -26,7 +28,7 @@ const addTwoNums = (num1, num2) => {
        // res.send(`Sum of two numbers is ${result}`);
     })
 
-    //REST - POST (using body param)
+    // method for subtracting 2 numbers
     const subTwoNums = (num3, num4) => {
         var number1=parseInt(num3);
         var number2=parseInt(num4);
@@ -34,6 +36,7 @@ const addTwoNums = (num1, num2) => {
         return result;
          }
          
+         //REST - POST (using body param)
          app.post("/subTwoNumbers",(req,res)=>{
              var num3=req.body.num3;
              var num4=req.body.num4;
@@ -43,10 +46,25 @@ const addTwoNums = (num1, num2) => {
             
             
          })
-       
-   
-  
 
+      //method to multiply 2 numbers  
+    const multiplyTwoNums = (num5, num6) => {
+        var number1=parseInt(num5);
+        var number2=parseInt(num6);
+        var result= number1*number2;
+        return result;
+         }
+         
+         //REST - PUT (using body param)
+         app.put("/multiplyTwoNumbers",(req,res)=>{
+             var num5=req.body.num5;
+             var num6=req.body.num6;
+             var result= multiplyTwoNums(num5,num6);
+             
+             res.json({statusCode:200,data:result,message:"Success"})
+         })
+
+    
 var port=process.env.port||3000;
 
 app.listen(port,()=>{
